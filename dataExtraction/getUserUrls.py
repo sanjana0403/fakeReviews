227a643c76da44e5e46cf034b32b500500ea60fd
sanjana <sanjana17708@gmail.com>
2018-02-11 19:19:38 +0530

    extracting user urls

--- dataExtraction/getUserUrls.py
+++ dataExtraction/getUserUrls.py
@@ -4,13 +4,10 @@ from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
-
-
-# TODO implement
-browser = webdriver.Chrome("/usr/lib/chromium-browser/chromedriver") #################### change here
+browser = webdriver.Chrome('/usr/lib/chromium-browser/chromedriver') #################### change here
 
 file = open("rest_list.txt","r")
-fout = open("user_url.txt",'w')
+fout = open("user_url.txt",'a')
 
 io = 1
 for io in range(100) :
@@ -22,18 +19,16 @@ for io in range(100) :
 	allRevbutton = browser.find_element_by_xpath('//*[@id="selectors"]/a[2]')
 	browser.execute_script("arguments[0].scrollIntoView(true);", allRevbutton);
 	browser.execute_script("window.scrollBy(0,-70)");
-	print allRevbutton.text
+	# print allRevbutton.text
 	allRevbutton.click()
 		
-	print "hello"
 	time.sleep(5)
-	print "slept"
 	
 	while True:
 	    try:
 	    	loadMoreButton = browser.find_element_by_css_selector('div.load-more.bold.ttupper.tac.cursor-pointer.fontsize2')
 	        time.sleep(2)
-	        print loadMoreButton.text
+	        # print loadMoreButton.text
 	        browser.execute_script("arguments[0].scrollIntoView(true);", loadMoreButton);
 	        browser.execute_script("window.scrollBy(0,-100)");
 	        loadMoreButton.click()
@@ -41,18 +36,17 @@ for io in range(100) :
 	    except Exception as e:
 	        break
 
-	print "finding"
+	# print "finding"
 	
 	results = browser.find_elements_by_css_selector('div.header.nowrap.ui.left > a')
 	
-	print "found"
+	# print "found"
 
 	for p in results:
 		user_url = p.get_attribute('href')
-		print user_url
+		# print user_url
 		fout.write(user_url)
 		fout.write("\n")
 
 
 browser.quit()
-    
